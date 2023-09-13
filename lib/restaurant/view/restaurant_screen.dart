3e6a9ebbffbb8f9cf758a0a817626717e77b92f0
WooHyun-P <dkqnwlnd@naver.com>
2023-09-13 21:08:26 +0900

    pagination UI 작업까지 마무리

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -11,36 +11,83 @@ import '../../common/model/cursor_pagination_model.dart';
 import '../model/restaurant_model.dart';
 import '../repository/restaurant_repository.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+// 리스트의 마지막 아이템 직전의 상태를 알기 위해 StatefulWidget 으로 변경함
+// ScrollController 를 사용하기 위해서임
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController _controller = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // _scrollController 의 특정 값이 변경될 때마다 호출되는 함수
+    _controller.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    // 현재 위치가 최대 길이보다 조금 덜되는 위치까지 왔다면
+    // 새로운 데이터를 추가 요청
+    // 바닥에서부터 300 픽셀 위의 스크롤을 넘게되면 실행할 것임
+    if (_controller.offset > _controller.position.maxScrollExtent - 300) {
+      ref.read(restaurantProvider.notifier).paginate(
+        fetchMore: true,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final resp = ref.watch(restaurantProvider);
 
     // 잘못된 예외처리임!! 지금은 이렇게밖에 처리 못하기때문..
     // 에러인지 아닌지 판단하는 로직이 필요함
     switch (resp.runtimeType) {
+      // 첫 로딩일 때
       case CursorPaginationLoading:
         return Center(
           child: CircularProgressIndicator(),
         );
+      // 에러일 때
       case CursorPaginationError:
         return Center(
           child: Text('데이터를 불러오는데 실패했습니다.'),
         );
     }
 
-    // TODO: 바꿔야됨!!
-    final data = (resp as CursorPagination<RestaurantModel>).data;
+    final cp = resp as CursorPagination;
+    if (resp.runtimeType is CursorPaginationFetchingMore) {
 
+    } else if (resp.runtimeType is CursorPaginationReFetch) {
+
+    } else {
+
+    }
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: ListView.separated(
-        itemCount: data.length,
+        controller: _controller,
+
+        itemCount: cp.data.length + 1,
         itemBuilder: (_, index) {
-          final curItem = data[index];
+          if (index == cp.data.length) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Center(
+                child: resp is CursorPaginationFetchingMore ?
+                  CircularProgressIndicator() : null,
+              ),
+            );
+          }
+
+          final curItem = cp.data[index];
 
           return GestureDetector(
               onTap: () {
